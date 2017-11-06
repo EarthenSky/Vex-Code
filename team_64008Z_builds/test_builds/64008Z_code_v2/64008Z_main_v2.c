1aaa980796569f488f6e77616bcf1e467d361fc7
EarthenSky <gabegames27@gmail.com>
2017-11-06 12:22:23 -0800

    only one script D:

--- team_64008Z_builds/test_builds/64008Z_code_v2/64008Z_main_v2.c
+++ team_64008Z_builds/test_builds/64008Z_code_v2/64008Z_main_v2.c
@@ -5,25 +5,19 @@
 #pragma config(Sensor, in1,    gyro,           	sensorGyro)
 #pragma config(Sensor, in2,    accelX,         	sensorAccelerometer)
 #pragma config(Sensor, in3,    accelY,         	sensorAccelerometer)
-
 #pragma config(Sensor, dgtl1,  enc1,           	sensorQuadEncoder)
 #pragma config(Sensor, dgtl3,  enc2,           	sensorQuadEncoder)
 #pragma config(Sensor, dgtl5,  handsUp,        	sensorTouch)
 #pragma config(Sensor, dgtl6,  handsDown,      	sensorTouch)
-
-/*Motors*/
 #pragma config(Motor,  port1,  clawMotor,      	tmotorVex393_HBridge, openLoop)
-
 #pragma config(Motor,  port2,  frontRightMotor,	tmotorVex393_MC29, openLoop, reversed)
 #pragma config(Motor,  port3,  backRightMotor, 	tmotorVex393_MC29, openLoop, reversed)
 #pragma config(Motor,  port4,  frontLeftMotor, 	tmotorVex393_MC29, openLoop)
 #pragma config(Motor,  port5,  backLeftMotor, 	tmotorVex393_MC29, openLoop)
-
 #pragma config(Motor,  port6,  coneArmTopR, 	  tmotorVex393_MC29, openLoop, reversed)
 #pragma config(Motor,  port6,  coneArmBottomR, 	tmotorVex393_MC29, openLoop, reversed)
 #pragma config(Motor,  port7,  coneArmTopL,  	  tmotorVex393_MC29, openLoop)
 #pragma config(Motor,  port8,  coneArmBottomL, 	tmotorVex393_MC29, openLoop)
-
 #pragma config(Motor,  port10,  handMotors,     tmotorVex393_HBridge, openLoop)
 
 /*Compo Init*/
@@ -67,6 +61,101 @@ void setConePickUpSpeed (int val=0) {
 	//right side
 	motor[coneArmTopR] = val;
 	motor[coneArmBottomR] = val;
+}
+
+///holds autonomous commands and main function.
+///
+
+///moves goal arms up or down based on value of direction. (positive or negitave)
+void autoMoveGoalArms(int direction) {
+	direction > 0 ? motor[handMotors] = 127 : motor[handMotors] = -127;
+	wait1Msec(1250);
+	motor[handMotors] = 0;
+}
+
+///time is in ms, speeds fromn -127 to 127.
+void moveLeftRightFor(int time, int leftSpeed, int rightSpeed) {
+	//left side
+	motor[frontLeftMotor] = leftSpeed;
+	motor[backLeftMotor] = leftSpeed;
+	//right side
+	motor[frontRightMotor] = rightSpeed;
+	motor[backRightMotor] = rightSpeed;
+
+  wait1Msec(time);
+
+  //left side
+	motor[frontLeftMotor] = 0;
+	motor[backLeftMotor] = 0;
+	//right side
+	motor[frontRightMotor] = 0;
+	motor[backRightMotor] = 0;
+}
+
+//rotates until over a certain gyro value.  //TODO: remove mod?
+void rotateUntilDegrees(float degrees, int speed, float mod=1) {
+  setLeftRightMoveSpeed(speed, -speed);
+
+  if (degrees > 0) {
+    WaitUntil(abs(SensorValue[gyro]) >= degrees - mod);
+  }
+  else {
+    WaitUntil(abs(SensorValue[gyro]) <= degrees + mod);
+  }
+
+  setLeftRightMoveSpeed();
+}
+
+///runs autonomous
+void runAuto() {
+  /*Drop Goal*/
+	autoMoveGoalArms(-127);
+
+	/*Move Forwards*/
+	moveLeftRightFor(2200, 70, 70);
+
+	/*Pick Up Goal*/
+	autoMoveGoalArms(127);
+
+	/*Pull Out*/
+	moveLeftRightFor(2200, -70, -70);
+
+	/*Rotate ~ >180 Degrees*/
+  rotateUntilDegrees(18 * 7.5, 110);
+
+	/*Move to Place Goal*/
+	moveLeftRightFor(1500, 100, 100);
+
+	/*Drop Goal*/
+	autoMoveGoalArms(-127);
+
+	/*Move Away From Goal.*/
+  pullBack();
+}
+
+void pullBack () {
+  //small jolt
+	moveLeftRightFor(150, -127, -127);
+	Wait1Msec(150);
+
+  //small jolt
+	moveLeftRightFor(150, -127, -127);
+	Wait1Msec(150);
+
+  //large pull
+	moveLeftRightFor(1500, -127, -127);
+	Wait1Msec(150);
+
+  //large pull
+	moveLeftRightFor(1500, -127, -127);
+  Wait1Msec(150);
+
+  //large pull
+	moveLeftRightFor(1500, -127, -127);
+  Wait1Msec(150);
+
+  //large pull
+	moveLeftRightFor(3000, -127, -127);
 }
 
 void pre_auton() {
